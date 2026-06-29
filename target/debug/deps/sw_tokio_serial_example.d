3a329786cd5f7a944f399e09a609054e03492641
AmirMahdi <amirmahdi.hassanzadeh@outlook.com>
2026-06-29 21:28:27 +0330

    init

---
+++ target/debug/deps/sw_tokio_serial_example.d
@@ -0,0 +1,5 @@
+C:\Users\AmirMahdi\Desktop\sw_tokio_serial_example\target\debug\deps\sw_tokio_serial_example.d: src\main.rs
+
+C:\Users\AmirMahdi\Desktop\sw_tokio_serial_example\target\debug\deps\sw_tokio_serial_example.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/sw_tokio_serial_example.d
@@ -0,0 +1,1 @@
+C:\Users\AmirMahdi\Desktop\sw_tokio_serial_example\target\debug\sw_tokio_serial_example.exe: C:\Users\AmirMahdi\Desktop\sw_tokio_serial_example\src\main.rs
